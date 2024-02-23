fbe8e608ebb242ba0788b850a1f225e945d2fdd6
TommyLamar <73857259+TommyLamar@users.noreply.github.com>
2024-02-22 20:51:38 -0500

    Update SCPFuncs.py

--- main/fileTransfer/SCPFuncs.py
+++ main/fileTransfer/SCPFuncs.py
@@ -2,26 +2,26 @@ from paramiko import SSHClient
 from scp import SCPClient
 
 
-def upload(host, localPath, remotePath, port=-1):
+def upload(host, localPath, remotePath, user="ubuntu", password="turtlebot", port=-1):
     ssh = SSHClient()
     ssh.load_system_host_keys()
     if port == -1:
-        ssh.connect(host)
+        ssh.connect(hostname=host, username=user, password=password)
     else:
-        ssh.connect(host, port)
+        ssh.connect(hostname=host, username=user, password=password, port=port)
 
     scp = SCPClient(ssh.get_transport())
     scp.put(localPath, recursive=True, remote_path=remotePath)
     scp.close()
 
 
-def download(host, localPath, remotePath, port=-1):
+def download(host, localPath, remotePath, user="ubuntu", password="turtlebot", port=-1):
     ssh = SSHClient()
     ssh.load_system_host_keys()
     if port == -1:
-        ssh.connect(host)
+        ssh.connect(hostname=host, username=user, password=password)
     else:
-        ssh.connect(host, port)
+        ssh.connect(hostname=host, username=user, password=password, port=port)
 
     scp = SCPClient(ssh.get_transport())
     scp.get(remote_path=remotePath, local_path=localPath, recursive=True)
